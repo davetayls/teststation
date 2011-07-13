9b9e9f1c803c04ee1a96653631345dcf4d066435
davetayls <dave@the-taylors.org>
2011-07-13 21:00:46 +0100

    added a few more particle experiments

--- particles/particle.js
+++ particles/particle.js
@@ -15,28 +15,48 @@ function Particle(cx, properties){
     this.size = properties.size || 2;
     this.grow = properties.grow || 1; // 1 === no grow
     this.maxSize = properties.maxSize || 100;
+
+    // opacity
+    this.opacity = properties.opacity || 1;
+
+    // image
+    this.image = properties.image || null;
 }
 Particle.prototype = {
+    getVelocity: function(){
+    },
+    basicDrag: function(vel, drag){
+        return vel * drag; 
+    },
     frame: function(){
+        // drag
+        if (typeof this.drag === 'function') {
+            this.xVelocity = this.drag(this.xVelocity, 'x');
+            this.yVelocity = this.drag(this.yVelocity, 'y');
+        } else {
+            this.xVelocity = this.basicDrag(this.xVelocity, this.drag);
+            this.yVelocity = this.basicDrag(this.yVelocity, this.drag);
+        }
+
         // velocity
         this.x += this.xVelocity;
         this.y += this.yVelocity;
-
-        // drag
-        this.xVelocity = this.xVelocity * this.drag;
-        this.yVelocity = this.yVelocity * this.drag;
 
         // size
         this.size = this.size > this.maxSize ? this.size : this.size * this.grow;
     },
     render: function(){
-        this.cx.fillStyle = '#000000';
-        this.cx.fillRect(
-            this.x - (this.size / 2),
-            this.y - (this.size / 2),
-            this.size,
-            this.size
-        );
+        if (this.image) {
+            this.cx.drawImage(this.image, this.x, this.y);
+        } else {
+            this.cx.fillStyle = 'rgba(0,0,0,' + this.opacity + ')';
+            this.cx.fillRect(
+                this.x - (this.size / 2),
+                this.y - (this.size / 2),
+                this.size,
+                this.size
+            );
+        }
     }
 };
 
