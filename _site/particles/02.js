2b4b58af38329c7aa3a8a8e43de377de433165a4
davetayls <dave@the-taylors.org>
2011-12-02 15:06:06 +0000

    added face detection

---
+++ _site/particles/02.js
@@ -0,0 +1,68 @@
+			var container, stats;
+			var c, 
+                cx,
+                canvasWidth = window.innerWidth, 
+                canvasHeight = window.innerHeight,
+                particles = [];
+
+
+			function init() {
+
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
+
+				var info = document.createElement( 'div' );
+				info.style.position = 'absolute';
+				info.style.top = '10px';
+				info.style.width = '100%';
+				info.style.textAlign = 'center';
+				info.innerHTML = 'canvas particles';
+				container.appendChild( info );
+
+				stats = new Stats();
+				stats.domElement.style.position = 'absolute';
+				stats.domElement.style.top = '0px';
+				container.appendChild( stats.domElement );
+
+                c = document.createElement('canvas');
+                c.style.position = 'absolute';
+                c.style.top = 0;
+                c.style.left = 0;
+                c.width = canvasWidth;
+                c.height = canvasHeight;
+                container.appendChild(c);
+
+                cx = c.getContext('2d');
+			}
+
+			function render() {
+
+                cx.clearRect(0,0, canvasWidth, canvasHeight);
+
+                for (var i = 0; i < particles.length; i++) {
+                    var p = particles[i];
+                    p.render();
+                    p.frame();
+                }
+                if (particles.length < 5000) {
+                    particles.push(new Particle(cx, {
+                        x: canvasWidth / 2,
+                        y: canvasHeight / 2,
+                        xVelocity: (Math.random() * 10) -5, 
+                        yVelocity: (Math.random() * 10) -5,
+                        drag: 0.9 + (Math.random() * 0.1),
+                        grow: 1 + (Math.random() * 0.01)
+                    }));
+                }
+			}
+			function animate() {
+				requestAnimationFrame( animate );
+
+				render();
+				stats.update();
+			}
+
+
+			init();
+			animate();
+
